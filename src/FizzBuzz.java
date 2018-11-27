8cd898e773c27f09e6e6e88fe9940b71148cc869
janiece1988 <janiecejoneswms@gmail.com>
2018-11-26 20:49:31 -0500

    multiples of 3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@ public class FizzBuzz {
 
 	public static String answer(int i) {
 		// TODO Auto-generated method stub
-		if(i==3)
+		if(i%3==0)
 			return "Fizz";
 		return Integer.toString(i);
 	}
--- src/FizzTests.java
+++ src/FizzTests.java
@@ -14,9 +14,11 @@ public class FizzTests {
 		assertEquals("2", FizzBuzz.answer(2));
 	}
 	@Test
-	public void ThreeReturnsThree() {
+	public void MultiplesofThree() {
 		String expected = "Fizz";
 	    String actual = FizzBuzz.answer(3);
+		assertEquals(expected, actual);
+		actual = FizzBuzz.answer(6);
 		assertEquals(expected, actual);
 	}
 }
