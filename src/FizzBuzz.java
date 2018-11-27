62ec9d9110a943feca1efa67ab13be0b943da493
janiece1988 <janiecejoneswms@gmail.com>
2018-11-26 20:24:56 -0500

    refactored answer to return string instead of integer

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,9 +1,9 @@
 
 public class FizzBuzz {
 
-	public static int answer(int i) {
+	public static String answer(int i) {
 		// TODO Auto-generated method stub
-		return i;
+		return Integer.toString(i);
 	}
 
 }
--- src/FizzTests.java
+++ src/FizzTests.java
@@ -7,10 +7,10 @@ public class FizzTests {
 
 	@Test
 	public void OneReturnsOne() {
-		assertEquals(1, FizzBuzz.answer(1));
+		assertEquals("1", FizzBuzz.answer(1));
 	}
 	@Test
 	public void TwoReturnsTwo() {
-		assertEquals(2, FizzBuzz.answer(2));
+		assertEquals("2", FizzBuzz.answer(2));
 	}
 }
